8aa7431cbaad0b4715b4028e3f5f37a8a2b3d859
Thomas23277 <aguerot300@gmail.com>
2026-05-21 21:51:40 -0300

    Update

--- Backend/src/main/java/com/foodstore/htmeleros/auth/controller/AuthController.java
+++ Backend/src/main/java/com/foodstore/htmeleros/auth/controller/AuthController.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
